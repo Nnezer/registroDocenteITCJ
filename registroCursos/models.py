a5f5e08c242818b5cc28901c7af86955ea31decc
Nnezer <Nnezer.23@gmail.com>
2018-09-30 13:13:38 -0600

    demo

--- registroCursos/models.py
+++ registroCursos/models.py
@@ -53,9 +53,8 @@ class Instructor(models.Model):
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
-    alumnos = models.ManyToManyField(Alumno, through='Clase')
-    instructor = models.ForeignKey(
-        Instructor, on_delete=models.CASCADE, verbose_name="Instructor", blank=True)
+    alumnos = models.ManyToManyField(Alumno)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, verbose_name="Instructor", blank=True)
     semestre = models.CharField(max_length=20, choices=semestres, default='')
     anno = models.IntegerField()
     diaInicio = models.DateField()
@@ -71,14 +70,3 @@ class Curso(models.Model):
         db_table = 'Curso'
 
 
-class Clase(models.Model):
-    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    aprobado = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.curso.nombre
-
-    class Meta:
-
-        db_table = 'curso_alumnos'
