8116c15c12660e321cf5a5b837f6ce60cffca72b
Nnezer <Nnezer.23@gmail.com>
2018-09-29 21:36:29 -0600

    demo

--- sistemaDocenteITCJ/settings.py
+++ sistemaDocenteITCJ/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,5 +140,3 @@ LOGOUT_REDIRECT_URL = 'home'
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
