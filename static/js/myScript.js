92cd160df83a6ab331a1ec8cbe40a85b15b9b937
Nnezer <Nnezer.23@gmail.com>
2018-10-15 13:41:33 -0600

    demo

--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,10 +1,10 @@
-<script>
-    function myFunction() {
-    var x = document.getElementById("Demo");
-    if (x.className.indexOf("w3-show") == -1) {
+ 
+    function myFunction(id) {
+            var x = document.getElementById(id);
+            if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
     }
 }
-</script>
+     
